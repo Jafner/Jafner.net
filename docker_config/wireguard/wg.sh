254a2ed7608c9800467fbd93abfba1bbeacf4bbf
Jafner <jafner425@gmail.com>
2021-07-11 15:52:02 -0700

    Working wireguard

--- docker_config/wireguard/wg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# PEERS=$1
-# sed -i "s/PEERS=.*/PEERS=$PEERS/g" docker-compose.yml
-docker container restart wireguard
-rclone copy ./config/ gdrive: --filter "- /coredns/" --filter "- /templates/" --filter "- /server/" --filter "+ peer*.{conf,png}" --filter "- *" 
