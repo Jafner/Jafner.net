0256932ee9bbf97e0e6680465344967db32a4746
Jafner <40403594+Jafner@users.noreply.github.com>
2021-11-16 10:32:08 -0800

    Add virtualization scripts

--- dotfiles_github/.scripts/window_reposition.sh
+++ dotfiles_github/.scripts/window_reposition.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-sleep 6
+#sleep 6
 
-#[Ferdi]
 WINDOW_ID=$(xdotool search --onlyvisible --name ferdi)
-xdotool windowsize $WINDOW_ID 1262 1383
-xdotool windowmove $WINDOW_ID 6406 45
+xdotool windowsize $WINDOW_ID 1262 1317
+xdotool windowmove $WINDOW_ID 6406 11
 
-#[PulseEffects]
 WINDOW_ID=$(xdotool search --onlyvisible --name pulseeffects)
-xdotool windowsize $WINDOW_ID 1320 760
-xdotool windowmove $WINDOW_ID 5100 703
+xdotool windowsize $WINDOW_ID 1314 715 
+xdotool windowmove $WINDOW_ID 5106 676
 
-xdotool key XF86AudioPlay
+WINDOW_ID=$(xdotool search --onlyvisible --name spot)
+xdotool windowsize $WINDOW_ID 1290 704
+xdotool windowmove $WINDOW_ID 5118 0
+
