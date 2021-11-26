462936ac2daa5a33731e6e4085ac510d8f4294df
Jafner <40403594+Jafner@users.noreply.github.com>
2021-11-25 21:54:25 -0800

    Install xdotool during pamidi install

--- dotfiles_github/install_pamidi.sh
+++ dotfiles_github/install_pamidi.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+set -x
 git clone https://github.com/Jafner/pamidi.git ~/Git/pamidi
 cd ~/Git/pamidi/
+sudo pacman -Sy xdotool
 chmod +x install_daemon_systemd.sh
 ./install_daemon_systemd.sh
