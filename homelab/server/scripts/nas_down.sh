0ac9e9dae33dc923f81db7f4707ca997a9435847
Joey Hafner <jafner425@gmail.com>
2022-08-18 10:44:53 -0700

    Grep the variable, not the actual paths

--- homelab/server/scripts/nas_down.sh
+++ homelab/server/scripts/nas_down.sh
@@ -5,7 +5,7 @@ do
     CONTAINER_NAME=$(docker ps -aq --filter "id=$CONTAINER_ID" --format '{{.Names}}') # get the container's name
     CONTAINER_MOUNTS=$(docker inspect --format '{{range .Mounts}}{{println .Source}}{{end}}' $CONTAINER_ID) # print the container's volume mounts
     echo "======== CHECKING $CONTAINER_NAME ========"
-    if grep -q /mnt/nas "$CONTAINER_MOUNTS"
+    if grep -q /mnt/nas $(echo "$CONTAINER_MOUNTS")
     then
         echo "IS NAS DEPENDENT"
     else
