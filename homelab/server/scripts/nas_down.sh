926035338386d7bfde5c6a49e38595cbfb095097
Joey Hafner <jafner425@gmail.com>
2022-08-18 10:32:14 -0700

    Init nas_down.sh (not tested)

---
+++ homelab/server/scripts/nas_down.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for CONTAINER_ID in $(docker ps -aq) # get list of all loaded containers (running and stopped)
+do 
+    CONTAINER_NAME=$(docker ps -aq --filter "id=$CONTAINER_ID" --format '{{.Names}}') # get the container's name
+    CONTAINER_MOUNTS=$(docker inspect --format '{{range .Mounts}}{{println .Source}}{{end}}' $CONTAINER_ID) # print the container's volume mounts
+    echo "$CONTAINER_NAME mounts:"
+    echo $CONTAINER_MOUNTS
+done
