6b71554d5ba406928edf4230573d46ad8e8a361c
Joey Hafner <jafner425@gmail.com>
2022-08-18 10:46:27 -0700

    Debug echo status of last command

--- homelab/server/scripts/nas_down.sh
+++ homelab/server/scripts/nas_down.sh
@@ -5,10 +5,6 @@ do
     CONTAINER_NAME=$(docker ps -aq --filter "id=$CONTAINER_ID" --format '{{.Names}}') # get the container's name
     CONTAINER_MOUNTS=$(docker inspect --format '{{range .Mounts}}{{println .Source}}{{end}}' $CONTAINER_ID) # print the container's volume mounts
     echo "======== CHECKING $CONTAINER_NAME ========"
-    if grep -q /mnt/nas $(echo "$CONTAINER_MOUNTS")
-    then
-        echo "IS NAS DEPENDENT"
-    else
-        echo "IS NOT NAS DEPENDENT"
-    fi
+    $(echo "CONTAINER_MOUNTS" | grep /mnt/nas)
+    echo $?
 done
