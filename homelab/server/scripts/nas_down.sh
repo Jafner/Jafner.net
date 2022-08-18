d147e5330e492f47b2734b806a5e73db15eac9ff
Joey Hafner <jafner425@gmail.com>
2022-08-18 10:34:33 -0700

    Add nas mount grep

--- homelab/server/scripts/nas_down.sh
+++ homelab/server/scripts/nas_down.sh
@@ -4,6 +4,6 @@ for CONTAINER_ID in $(docker ps -aq) # get list of all loaded containers (runnin
 do 
     CONTAINER_NAME=$(docker ps -aq --filter "id=$CONTAINER_ID" --format '{{.Names}}') # get the container's name
     CONTAINER_MOUNTS=$(docker inspect --format '{{range .Mounts}}{{println .Source}}{{end}}' $CONTAINER_ID) # print the container's volume mounts
-    echo "$CONTAINER_NAME mounts:"
-    echo $CONTAINER_MOUNTS
+    echo "======== CHECKING $CONTAINER_NAME ========"
+    echo $CONTAINER_MOUNTS | grep /mnt/nas
 done
