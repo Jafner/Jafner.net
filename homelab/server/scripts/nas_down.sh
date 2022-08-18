10e9ad34f765044631a03f68181c306e9a5eaadc
Joey Hafner <jafner425@gmail.com>
2022-08-18 10:39:30 -0700

    Syntax error

--- homelab/server/scripts/nas_down.sh
+++ homelab/server/scripts/nas_down.sh
@@ -5,7 +5,7 @@ do
     CONTAINER_NAME=$(docker ps -aq --filter "id=$CONTAINER_ID" --format '{{.Names}}') # get the container's name
     CONTAINER_MOUNTS=$(docker inspect --format '{{range .Mounts}}{{println .Source}}{{end}}' $CONTAINER_ID) # print the container's volume mounts
     echo "======== CHECKING $CONTAINER_NAME ========"
-    if $(echo "$CONTAINER_MOUNTS" | grep /mnt/nas)
+    if $(echo "$CONTAINER_MOUNTS" | grep /mnt/nas); then
         echo "IS NAS DEPENDENT"
     else
         echo "IS NOT NAS DEPENDENT"
