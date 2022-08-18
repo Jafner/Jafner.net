b7b489da514e7779745a439c39be904073abd79d
Joey Hafner <jafner425@gmail.com>
2022-08-18 10:36:57 -0700

    Per: https://stackoverflow.com/questions/22101778/how-to-preserve-line-breaks-when-storing-command-output-to-a-variable

--- homelab/server/scripts/nas_down.sh
+++ homelab/server/scripts/nas_down.sh
@@ -5,5 +5,5 @@ do
     CONTAINER_NAME=$(docker ps -aq --filter "id=$CONTAINER_ID" --format '{{.Names}}') # get the container's name
     CONTAINER_MOUNTS=$(docker inspect --format '{{range .Mounts}}{{println .Source}}{{end}}' $CONTAINER_ID) # print the container's volume mounts
     echo "======== CHECKING $CONTAINER_NAME ========"
-    echo $CONTAINER_MOUNTS | grep /mnt/nas
+    echo "$CONTAINER_MOUNTS" | grep /mnt/nas
 done
