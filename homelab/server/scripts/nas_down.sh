f58ea79846cc08a01f390b12173936cf06fe94eb
Joey Hafner <jafner425@gmail.com>
2022-08-18 10:38:55 -0700

    Add bool check for nas dependence

--- homelab/server/scripts/nas_down.sh
+++ homelab/server/scripts/nas_down.sh
@@ -5,5 +5,9 @@ do
     CONTAINER_NAME=$(docker ps -aq --filter "id=$CONTAINER_ID" --format '{{.Names}}') # get the container's name
     CONTAINER_MOUNTS=$(docker inspect --format '{{range .Mounts}}{{println .Source}}{{end}}' $CONTAINER_ID) # print the container's volume mounts
     echo "======== CHECKING $CONTAINER_NAME ========"
-    echo "$CONTAINER_MOUNTS" | grep /mnt/nas
+    if $(echo "$CONTAINER_MOUNTS" | grep /mnt/nas)
+        echo "IS NAS DEPENDENT"
+    else
+        echo "IS NOT NAS DEPENDENT"
+    fi
 done
