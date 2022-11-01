ef3c7a1ba6ce22601acae52d141ee81e003e6261
Joey Hafner <jafner425@gmail.com>
2022-11-01 15:58:24 -0700

    Preserve current working directory

--- homelab/server/scripts/apply_minecraft_server_list.sh
+++ homelab/server/scripts/apply_minecraft_server_list.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+DIR=$(pwd)
 # Apply updated EXPORT_SERVERS
 cd /home/joey/homelab/server/config/monitoring && docker-compose up -d --force-recreate exporter-minecraft
 
 # Apply updated router mappings
 cd /home/joey/homelab/server/config/minecraft && docker-compose up -d --force-recreate router
+
+cd $DIR
