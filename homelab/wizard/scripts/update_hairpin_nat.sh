9ef90a92acd71466084bbbabe6d069745ff2f2f1
Joey Hafner <jafner425@gmail.com>
2023-10-16 22:37:13 -0700

    Update hairpin nat script

--- homelab/wizard/scripts/update_hairpin_nat.sh
+++ homelab/wizard/scripts/update_hairpin_nat.sh
@@ -4,16 +4,17 @@ source /opt/vyatta/etc/functions/script-template
 # create copies of config for modification and backup
 cp /config/config.boot /config/config.new
 cp /config/config.boot /config/backups/config.bk-$(date +%Y-%m-%d)
-# get current public IP
-NEW_IP=$(run show interfaces pppoe pppoe1 | grep "inet\s" | cut -d ' ' -f 6)
-# get old public IP
-OLD_IP=$(run show nat destination rules | grep 1100 | head -1 | cut -d ' ' -f 8)
-
-# edit the config.new file
-sed -i "s/$OLD_IP/$NEW_IP/" /config/config.new
 
 # switch mode to config
 configure
+
+# get current public IP
+NEW_IP=$(run show interfaces pppoe pppoe1 | grep "inet\s" | cut -d ' ' -f 6)
+# get old public IP
+OLD_IP=$(run show nat destination rules | grep 1100 | head -1 | tr -s ' ' | cut -d' ' -f 3)
+
+# edit the config.new file
+sed -i "s/$OLD_IP/$NEW_IP/" /config/config.new
 
 # apply the new config
 load /config/config.new
