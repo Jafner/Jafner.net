1c2e275b750a9f8535ce4e861f8fd12a4f19ed47
Joey Hafner <jafner425@gmail.com>
2024-02-14 10:37:43 -0800

    Init new, split, commands-based configuration for wizard to ease automated deployment

---
+++ homelab/wizard/config/configure.sh
@@ -0,0 +1,16 @@
+#!/bin/vbash
+
+if [ "$(id -g -n)" != 'vyattacfg' ] ; then
+    exec sg vyattacfg -c "/bin/vbash $(readlink -f $0) $@"
+fi
+
+source /opt/vyatta/etc/functions/script-template
+
+. firewall.sh
+. interfaces.sh
+. nat.sh
+. qos.sh
+. service.sh
+. system.sh
+
+exit
