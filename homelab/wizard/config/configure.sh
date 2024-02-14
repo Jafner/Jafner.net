5f4d405c23db4e5f357e04e91817c15f13b444f1
Joey Hafner <jafner425@gmail.com>
2024-02-14 11:07:57 -0800

    Add basic error handling to configure.sh Add newline to end of test.sh

--- homelab/wizard/config/configure.sh
+++ homelab/wizard/config/configure.sh
@@ -8,15 +8,58 @@ source /opt/vyatta/etc/functions/script-template
 
 configure
 
-source test.sh
-#source firewall.sh
-#source interfaces.sh
-#source nat.sh
-#source qos.sh
-#source service.sh
-#source system.sh
+echo "===== Configure firewall... ====="
+{ 
+    source firewall.sh 
+} || { 
+    echo "===== Failed to configure firewall =====" 
+}
+
+echo "===== Configure interfaces... ====="   
+{ 
+    source interfaces.sh 
+} || { 
+    echo "===== Failed to configure interfaces =====" 
+}
+
+echo "===== Configure NAT... ====="
+{ 
+    source nat.sh 
+} || { 
+    echo "===== Failed to configure NAT =====" 
+}
+
+echo "===== Configure QoS... ====="
+{ 
+    source qos.sh 
+} || { 
+    echo "===== Failed to configure QoS =====" 
+}
+
+
+echo "===== Configure services... ====="
+{ 
+    source service.sh 
+} || { 
+    echo "===== Failed to configure services =====" 
+}
+
+
+echo "===== Configure system... ====="
+{ 
+    source system.sh 
+} || { 
+    echo "===== Failed to configure system =====" 
+}
+
 
 compare
-commit
-save
+
+{ # try commit, save
+    commit && save
+} || { # catch, exit discard and print a very basic error message
+    exit discard
+    echo "Failed to commit and save the configuration."
+}
+
 exit
