4f090e1d9a4b97b1124de1f77b7d7b6ffb1975be
Joey Hafner <jafner425@gmail.com>
2024-02-20 11:38:30 -0800

    Fix using wrong docker compose command

--- homelab/fighter/scripts/shutdown.sh
+++ homelab/fighter/scripts/shutdown.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for stack in /home/admin/homelab/fighter/config/*; do cd $stack && docker-compose down; done
+for stack in /home/admin/homelab/fighter/config/*; do cd $stack && docker compose down; done
 sudo umount /mnt/iscsi
 sudo iscsiadm --mode node --targetname "iqn.2020-03.net.jafner:fighter" --portal "192.168.1.10:3260" --logout
 for mount in media calibre-web torrenting av; do sudo umount /mnt/nas/$mount; done
