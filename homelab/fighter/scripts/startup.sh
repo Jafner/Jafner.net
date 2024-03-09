ecf6d86d1c0af974b854ed535a2c1a45c288c130
Joey Hafner <jafner425@gmail.com>
2024-03-09 01:04:44 -0800

    Update fighter startup/shutdown scripts

--- homelab/fighter/scripts/startup.sh
+++ homelab/fighter/scripts/startup.sh
@@ -78,11 +78,12 @@ for stack in /home/admin/homelab/fighter/config/*; do
     if ! $MISSING_SHARES; then 
         echo  "  ====== Bringing up"
         STACKS_ONLINE+="$(echo $stack | xargs basename)\n"
-        echo -n "    ====== Time: "
+        echo -n "  ======== Time: "
         ( time docker compose --progress quiet up -d ) 2>&1 | grep real | cut -f 2
         echo "  ====== Done!"
     else
         echo "  ====== Missing needed network shares. Skipping."
+        STACKS_SKIPPED+="$(echo $stack | xargs basename)\n"
     fi
     
     cd /home/admin/homelab/fighter/config/
@@ -92,6 +93,8 @@ done
 
 echo "  ==== List of stacks online:"
 echo -e "$STACKS_ONLINE"
+echo "  ==== List of stacks skipped:"
+echo -e "$STACKS_SKIPPED"
 
 # extra thing because my keycloak healthcheck doesn't work properly
 echo "  ==== Wait 15s, then bring Keycloak forwardauth containers online"
