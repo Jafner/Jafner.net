b641cb9c81e8abd662acbda0cc3ea4b9295dc9c3
Joey Hafner <joey@jafner.net>
2024-07-05 12:02:54 -0700

    Init install script for ffmpeg script group

---
+++ dotfiles_gitea/setup-scripts/install-utility-scripts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install clip-it-and-ship-it
+# Install convert-to-av1
+# Install convert-to-mp4
+# Install convert-to-x264
+# Install send-to-zipline
+
+
+
+
+
+chmod +x ../utility-scripts/ffmpeg/*
+cp ../utility-scripts/ffmpeg/* /usr/bin/
