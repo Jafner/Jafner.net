7052b517fb296c72ec88b114a8b1092f1a087f8d
Joey Hafner <joey@jafner.net>
2024-08-16 14:31:58 -0700

    #3 Polish up the setup script, delete unused manual scripts

---
+++ homelab/.sops/age-setup.sh
@@ -0,0 +1,35 @@
+AGE_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Set up age keypair
+if [[ -f $HOME/.age/key ]]; then 
+    if ! cat ~/.bashrc | grep -q "export SOPS_AGE_KEY_FILE"; then
+        echo "Add this line to your shell profile (e.g. ~/.bashrc or ~/.zshrc):"
+        echo "export SOPS_AGE_KEY_FILE=$HOME/.age/key"
+    else
+        echo "SOPS_AGE_KEY_FILE: $SOPS_AGE_KEY_FILE"
+    fi
+else
+    mkdir -p $HOME/.age
+    HOST_CONFIG_DIR=$AGE_DIR/../$HOSTNAME/
+    mkdir -p $HOST_CONFIG_DIR
+    age-keygen -o $HOME/.age/key > $HOST_CONFIG_DIR/.age-pubkey
+    echo "Pubkey added to $HOST_CONFIG_DIR/.age-pubkey"
+    echo "If any secrets have already been committed for this host, re-encrypt them with the new pubkey as a recipient."
+fi
+
+# Configure the git filters
+git config --local filter.sops.smudge $AGE_DIR/decrypt-filter.sh
+git config --local filter.sops.clean $AGE_DIR/encrypt-filter.sh
+git config --local filter.sops.required true
+
+# Set up age keypair
+if [[ -f $HOME/.age/key ]]; then 
+    if ! cat ~/.bashrc | grep -q "export SOPS_AGE_KEY_FILE"; then
+        echo "Add this line to your shell profile (e.g. ~/.bashrc or ~/.zshrc):"
+        echo "export SOPS_AGE_KEY_FILE=$HOME/.age/key"
+    else
+        echo "SOPS_AGE_KEY_FILE: $SOPS_AGE_KEY_FILE"
+    fi
+else
+    echo "No age key file. Generate one."
+fi
