64d94faf77ec086df102e04daf31131e04b29c8f
Joey Hafner <joey@jafner.net>
2024-08-27 21:48:55 -0700

    Move local ai to Stack

--- projects/local-ai/ollama/modelfiles/.loadmodels.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for modelfile in /modelfiles/*; do 
-    echo -n "Running: '"
-    echo "ollama create \"$(basename $modelfile)\" -f \"$modelfile\"'"
-    ollama create "$(basename $modelfile)" -f "$modelfile"
-done
