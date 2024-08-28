16e3069d39e941233f9e9be43ba533fa553d956e
Joey Hafner <joey@jafner.net>
2024-08-28 13:14:39 -0700

    #3 Remove textconv filter so that we can see encrypted content when staged (peace of mind seeing the encrypted contents)

--- .sops/sops-setup.sh
+++ .sops/sops-setup.sh
@@ -21,4 +21,3 @@ fi
 git config --local filter.sops.smudge "$SOPS_DIR/decrypt-filter.sh %f"
 git config --local filter.sops.clean "$SOPS_DIR/encrypt-filter.sh %f"
 git config --local filter.sops.required true
-git config --local diff.sops.textconv "sops decrypt"
