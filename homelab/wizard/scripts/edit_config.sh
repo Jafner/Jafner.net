711773e5807e9b5b40eaf1f9e4639745dae63165
Joey Hafner <joey@jafner.net>
2024-08-29 00:00:24 -0700

    Add DIY DDNS script for Cloudflare to VyOS Clean out old, unused scripts

--- homelab/wizard/scripts/edit_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/vbash
-# https://docs.vyos.io/en/equuleus/automation/command-scripting.html
-source /opt/vyatta/etc/functions/script-template
-
-cp /config/config.boot /config/config.new
-nano /config/config.new
-configure
-load /config/config.new
