be11e36b464dbdd95ccd8016d846f093002ae04e
Joey Hafner <joey@jafner.net>
2024-09-10 12:46:57 -0700

    Remove SHA256 pins

--- nixos/hosts/xps13_9350/home.nix
+++ nixos/hosts/xps13_9350/home.nix
@@ -20,7 +20,6 @@
           owner = "zsh-users";
           repo = "zsh-autosuggestions";
           rev = "v0.7.0";
-          sha256 = "a411ef3e0992d4839f0732ebeb9823024afaaaa8";
         };
       }
       {
@@ -29,7 +28,6 @@
           owner = "zsh-users";
           repo = "zsh-completions";
           rev = "v0.35.0";
-          sha256 = "67921bc12502c1e7b0f156533fbac2cb51f6943d";
         };
       }
       {
@@ -38,7 +36,6 @@
           owner  = "zsh-users";
           repo  = "zsh-syntax-highlighting";
           rev   = "v0.8.0";
-          sha256= "db085e4661f6aafd24e5acb5b2e17e4dd5dddf3e";
         };
       }
       {
@@ -47,7 +44,6 @@
           owner = "jirutka";
           repo = "zsh-shift-select";
           rev = "v0.1.1";
-          sha256 = "47296f18c52e9cdff5ddf0c28a5cc8c88ef8696e";
         };
       }
     ];
