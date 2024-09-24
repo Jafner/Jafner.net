2fc07d035325fe8af9d402c7616c6d6c436de795
Joey Hafner <joey@jafner.net>
2024-09-24 13:41:47 -0700

    Colmena dev

--- nix/nix-lab/flake.nix
+++ nix/nix-lab/flake.nix
@@ -47,6 +47,7 @@
           };
         };
         time.timeZone = "America/Los_Angeles";
+        nix.settings.trusted-users = [ "root" "admin" ];
         boot.loader.systemd-boot.enable = true;
         boot.loader.efi.canTouchEfiVariables = true;
         system.stateVersion = "24.05";
