3026858fc42d32dd286714d47126e5e9e49f4e01
Joey Hafner <joey@jafner.net>
2024-10-14 14:16:29 -0700

    Switch to "media" nix file, add hyprland as home-manager import

--- nix/home-manager/users/joey.nix
+++ nix/home-manager/users/joey.nix
@@ -9,13 +9,13 @@
     ../app/browser/zen.nix
     ../app/flatpak/flatpak.nix
     ../app/git/git.nix
-    ../app/media/vlc.nix
+    ../app/media/media.nix
     ../app/messaging/discord.nix 
     ../app/obs-studio/obs-studio.nix
     ../app/bitwarden/bitwarden.nix
-    ../app/plasma-manager/plasma-manager.nix # This is coupled with the plasma wm
     ../app/sh/sh.nix # Consider splitting out to "terminal", "shell", and "cmd" or similar
     ../app/vscode/vscode.nix # Consider using a generalized "IDE" or "Editor" folder.
+    ../wm/hyprland/wm.nix
   ];
   
   # Desktop apps
