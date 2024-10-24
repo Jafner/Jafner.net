d85318e050129b0d9469803fc61815c4ecef68cf
Joey Hafner <joey@jafner.net>
2024-10-24 12:01:49 -0700

    Init OBS xdg.desktopEntries

--- nix/dungeon-master/home-manager/home.nix
+++ nix/dungeon-master/home-manager/home.nix
@@ -137,6 +137,23 @@
         ${pkgs-unstable.nixgl.nixVulkanIntel}/bin/nixVulkanIntel ${pkgs-unstable.obs-studio}/bin/obs "$@"
     '';
   };
+  xdg.desktopEntries."obs" = {
+    name = "OBS Studio";
+    genericName = "Streaming/Recording Software";
+    type = "Application";
+    comment = "Free and Open Source Streaming/Recording Software";
+    categories = [ "AudioVideo" "Recorder" ]; 
+    exec = "nixGL obs";
+    icon = pkgs.fetchurl { 
+      url = "https://upload.wikimedia.org/wikipedia/commons/d/d3/OBS_Studio_Logo.svg"; 
+      sha256 = "sha256-7kTlDSmknwahRiiSPZPU7Fs49q2ViSIPsI/1s8z8mIs="; 
+    }; 
+    terminal = false;
+    settings = {
+      StartupNotify = true;
+      StartupWMClass = "obs";
+    };
+  };
   programs.git = {
     enable = true;
     userName = "Joey Hafner";
