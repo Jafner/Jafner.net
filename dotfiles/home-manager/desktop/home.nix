27afcce0913c052d801e85cb23b9fa00072f6371
Joey Hafner <joey@jafner.net>
2024-11-02 18:11:06 -0700

    Add GoXLR service

--- dotfiles/home-manager/desktop/home.nix
+++ dotfiles/home-manager/desktop/home.nix
@@ -284,6 +284,17 @@
         ExecStart = "${pkgs-unstable.librespot}/bin/librespot --backend pulseaudio --system-cache /home/${vars.user.username}/.spotify -j";
       };
     };
+    goxlr-utility = {
+      Unit = {
+        Description = "Unofficial GoXLR App replacement for Linux, Windows and MacOS";
+        Documentation = [ "https://github.com/GoXLR-on-Linux/goxlr-utility" ];
+      };
+      Service = {
+        Restart = "always";
+        RestartSec = 30;
+        ExecStart = "${pkgs.goxlr-utility}/bin/goxlr-daemon";
+      };
+    };
   };
   home.enableNixpkgsReleaseCheck = false;
   home.preferXdgDirectories = true;
@@ -305,7 +316,6 @@
     ollama
     protonup-ng
     protonmail-bridge-gui
-    goxlr-utility
     obsidian
     gamepad-tool
     linuxKernel.packages.linux_6_11.xpadneo
