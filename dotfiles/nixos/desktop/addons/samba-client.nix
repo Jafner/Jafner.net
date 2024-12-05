fc76ff7f0f3bb44e4af9fc846906e91a5a76d68b
Joey Hafner <joey@jafner.net>
2024-12-05 14:18:05 -0800

    Configure SMB network shares

--- dotfiles/nixos/desktop/addons/samba-client.nix
+++ dotfiles/nixos/desktop/addons/samba-client.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [ cifs-utils ];
-
   fileSystems = 
     let 
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s"; 
