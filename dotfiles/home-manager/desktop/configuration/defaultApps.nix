594fcaed03157afb4b4a9b7e5de9b91d0cb977b1
Joey Hafner <joey@jafner.net>
2024-12-31 10:45:55 -0800

    implement let ... in {} block for categorical default applications

--- dotfiles/home-manager/desktop/configuration/defaultApps.nix
+++ dotfiles/home-manager/desktop/configuration/defaultApps.nix
@@ -1,14 +1,38 @@
 { ... }: {
-  xdg.mimeApps = { 
+  xdg.mimeApps = let
+    webBrowser = "zen.desktop";
+    emailClient = "proton-mail.desktop";
+
+    imageViewer = "org.kde.gwenview.desktop";
+    musicPlayer = "vlc.desktop";
+    videoPlayer = "vlc.desktop";
+
+    textEditor = "dev.zed.Zed.desktop";
+    docViewer = "zen.desktop";
+
+    fileManager = "org.kde.dolphin.desktop";
+    terminal = "org.kde.konsole.desktop";
+    archiveManager = "org.kde.ark.desktop";
+  in {
     enable = true;
     defaultApplications = {
-      "text/plain" = "codium.desktop";
-      "text/html" = "zen.desktop";
-      "x-scheme-handler/http" = "zen.desktop";
-      "x-scheme-handler/https" = "zen.desktop";
-      "x-scheme-handler/about" = "zen.desktop";
-      "x-scheme-handler/unknown" = "zen.desktop";
-      "video/*" = "vlc.desktop";
+      "x-scheme-handler/http" = "${webBrowser}";
+      "x-scheme-handler/https" = "${webBrowser}";
+      "x-scheme-handler/about" = "${webBrowser}";
+      "x-scheme-handler/unknown" = "${webBrowser}";
+      "application/json" = "${textEditor}";
+      "application/pdf" = "${webBrowser}";
+      "application/vnd.apple.keynote" = "${textEditor}";
+      "application/vnd.ms-publisher" = "${textEditor}";
+      "application/x-desktop" = "${textEditor}";
+      "application/x-executable" = "${textEditor}";
+      "text/css" = "${textEditor}";
+      "text/html" = "${textEditor}";
+      "text/plain" = "${textEditor}";
+      "video/mp4" = "${videoPlayer}";
+      "video/x-matroska" = "${videoPlayer}";
+    };
+    associations.added = {
     };
   };
 }
