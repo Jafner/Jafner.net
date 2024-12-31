85f7fc855a0b6516a67228ea870e10d3e7359aff
Joey Hafner <joey@jafner.net>
2024-12-31 10:49:00 -0800

    Enable system auto updates

--- dotfiles/nixos/desktop/sysconfig/nixos.nix
+++ dotfiles/nixos/desktop/sysconfig/nixos.nix
@@ -1,5 +1,5 @@
-{ pkgs, lib, ... }: {
-  nixpkgs.config.allowUnfree = true; 
+{ pkgs, lib, inputs, ... }: {
+  nixpkgs.config.allowUnfree = true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   programs.nix-ld = {
     enable = true;
@@ -7,4 +7,15 @@
   };
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   home-manager.backupFileExtension = "backup";
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L"
+    ];
+    dates = "04:00";
+    randomizedDelaySec = "30min";
+  };
 }
