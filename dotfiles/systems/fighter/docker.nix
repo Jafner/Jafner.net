6ff8e0467fa1e2da55c5641a0a275b958bd0200f
Joey Hafner <joey@jafner.net>
2025-01-31 22:28:47 -0800

    Enhancement: Move docker data root to `/docker`

--- dotfiles/systems/fighter/docker.nix
+++ dotfiles/systems/fighter/docker.nix
@@ -1,9 +1,11 @@
 { pkgs, sys, ... }: {
   virtualisation.docker = {
     enable = true;
+    daemon.settings.data-root = "/docker";
     rootless.enable = true;
     rootless.setSocketVariable = true;
   };
+
   users.users.${sys.username}.extraGroups = [ "docker" ];
   environment.systemPackages = [ pkgs.docker-compose ];
 }
