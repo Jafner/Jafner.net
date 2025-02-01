7778fa68ec75a08a74c99ba5f5da207111422d9c
Joey Hafner <joey@jafner.net>
2025-02-01 00:13:25 -0800

    Feature: Init stack.nix for keycloak.   - Rotate secrets for all components.   - Delete unused volume.

---
+++ homelab/stacks/keycloak/stack.nix
@@ -0,0 +1,41 @@
+{ sys, ... }: let stack = "keycloak"; in {
+  home-manager.users."${sys.username}".home.file = {
+    "${stack}" = {
+      enable = true;
+      recursive = true;
+      source = ./.;
+      target = "stacks/${stack}/";
+    };
+    "${stack}/.env" = {
+      enable = true;
+      text = ''
+        APPDATA=${sys.dataDirs.appdata}/${stack}
+      '';
+      target = "stacks/${stack}/.env";
+    };
+  };
+  sops.secrets."${stack}/keycloak" = { 
+    sopsFile = ./keycloak.secrets;
+    key = "";
+    mode = "0440";
+    owner = sys.username;
+  };
+  sops.secrets."${stack}/postgres" = { 
+    sopsFile = ./postgres.secrets;
+    key = "";
+    mode = "0440";
+    owner = sys.username;
+  };
+  sops.secrets."${stack}/forwardauth" = { 
+    sopsFile = ./forwardauth.secrets;
+    key = "";
+    mode = "0440";
+    owner = sys.username;
+  };
+  sops.secrets."${stack}/forwardauth-privileged" = { 
+    sopsFile = ./forwardauth-privileged.secrets;
+    key = "";
+    mode = "0440";
+    owner = sys.username;
+  };
+}
