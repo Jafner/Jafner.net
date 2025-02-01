e44cd12d6c365dd5320c82c792ebe8af388a7ed0
Joey Hafner <joey@jafner.net>
2025-01-31 22:53:20 -0800

    Feature: Add stack.nix for `autopirate`.

---
+++ homelab/stacks/autopirate/stack.nix
@@ -0,0 +1,30 @@
+{ sys, ... }: let stack = "coder"; in {
+  home-manager.users."${sys.username}".home.file = {
+    "${stack}" = {
+      enable = true;
+      recursive = true;
+      source = ./.;
+      target = "stacks/${stack}/";
+    };
+    "${stack}/.env" = {
+      enable = true;
+      text = ''
+        APPDATA=${sys.dataDirs.appdata}/autopirate
+        MOVIES_DIR=${sys.dataDirs.library.movies}
+        SHOWS_DIR=${sys.dataDirs.library.shows}
+        MUSIC_DIR=${sys.dataDirs.library.music}
+        BOOKS_DIR=${sys.dataDirs.library.books}
+        NZB_DIR=${sys.dataDirs.appdata}/torrenting/NZB
+        INCOMPLETE_NZB_DIR=${sys.dataDirs.appdata}/torrenting/NZB_incomplete
+        TRANSCODE_DIR=${sys.dataDirs.appdata}/tdarr
+      '';
+      target = "stacks/${stack}/.env";
+    };
+  };
+  sops.secrets."${stack}" = { 
+    sopsFile = ./secrets.env;
+    key = "";
+    mode = "0440";
+    owner = sys.username;
+  };
+}
