c502e196a43b63d9fad5df5d5f7f2c2c686127d1
Joey Hafner <joey@jafner.net>
2025-02-01 00:19:49 -0800

    Feature: Init stack.nix for unifi

---
+++ homelab/stacks/unifi/stack.nix
@@ -0,0 +1,15 @@
+{ sys, ... }: let stack = "unifi"; in {
+  home-manager.users."${sys.username}".home.file = {
+    "${stack}" = {
+      enable = true;
+      recursive = true;
+      source = ./.;
+      target = "stacks/${stack}/";
+    };
+    "${stack}/.env" = {
+      enable = true;
+      text = ''APPDATA=${sys.dataDirs.appdata}/${stack}'';
+      target = "stacks/${stack}/.env";
+    };
+  };
+}
