09a4d88e006e7bb68585c78827dfb688144def29
Joey Hafner <joey@jafner.net>
2025-01-31 23:54:42 -0800

    Feature: Init stack.nix for Gitea.   - Rotate gitea and postgres secrets.   - Small update to docs.   - Delete unused runner-related files.   - Delete runner.   - Move postgres data to `$APPDATA/postgres` Note: Primary Gitea instance on `gitea.jafner.tools` is running its own legacy config. This config will be used for a new `git.jafner.net` instance.

---
+++ homelab/stacks/gitea/stack.nix
@@ -0,0 +1,29 @@
+{ sys, ... }: let stack = "gitea"; in {
+  home-manager.users."${sys.username}".home.file = {
+    "${stack}" = {
+      enable = true;
+      recursive = true;
+      source = ./.;
+      target = "stacks/${stack}/";
+    };
+    "${stack}/.env" = {
+      enable = true;
+      text = ''
+        APPDATA=${sys.dataDirs.appdata}/${stack}
+      '';
+      target = "stacks/${stack}/.env";
+    };
+  };
+  sops.secrets."${stack}/gitea" = { 
+    sopsFile = ./gitea.secrets;
+    key = "";
+    mode = "0440";
+    owner = sys.username;
+  };
+  sops.secrets."${stack}/postgres" = { 
+    sopsFile = ./postgres.secrets;
+    key = "";
+    mode = "0440";
+    owner = sys.username;
+  };
+}
