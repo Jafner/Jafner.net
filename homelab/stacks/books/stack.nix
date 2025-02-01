d1436f849198e182d4ba79900a52dd8d0b0ab41d
Joey Hafner <joey@jafner.net>
2025-01-31 23:32:53 -0800

    Feature: Init stack.nix for books, rotate secrets.env

---
+++ homelab/stacks/books/stack.nix
@@ -0,0 +1,28 @@
+{ sys, ... }: let stack = "books"; in {
+  home-manager.users."${sys.username}".home.file = {
+    "${stack}" = {
+      enable = true;
+      recursive = true;
+      source = ./.;
+      target = "stacks/${stack}/";
+    };
+    "${stack}/.env" = {
+      enable = true;
+      text = ''
+        APPDATA=${sys.dataDirs.appdata}/books
+        EBOOKS_LIBRARY=${sys.dataDirs.library.books}/ebooks
+        AUDIOBOOKS_LIBRARY=${sys.dataDirs.library.books}/audiobooks
+        NZB_COMPLETED=${sys.dataDirs.appdata}/torrenting/NZB
+        NZB_INCOMPLETE=${sys.dataDirs.appdata}/torrenting/NZB_incomplete
+        LIBRARY_DIR=${sys.dataDirs.library.books}/Calibre
+      '';
+      target = "stacks/${stack}/.env";
+    };
+  };
+  sops.secrets."${stack}" = { 
+    sopsFile = ./secrets.env;
+    key = "";
+    mode = "0440";
+    owner = sys.username;
+  };
+}
