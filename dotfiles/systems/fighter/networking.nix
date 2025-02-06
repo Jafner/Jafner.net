6f2abb40ba78dfd1533906d55fb8531729ded73d
Joey Hafner <joey@jafner.net>
2025-02-06 15:32:36 -0800

    Fix: Enable DHCP client. Interface not coming up automatically on boot.

--- dotfiles/systems/fighter/networking.nix
+++ dotfiles/systems/fighter/networking.nix
@@ -3,7 +3,7 @@
     hostName = "fighter";
     defaultGateway = { address = "192.168.1.1"; interface = "enp3s0"; };
     interfaces."enp3s0" = {
-      useDHCP = false;
+      useDHCP = true;
       macAddress = "00:02:c9:56:bf:9a";
       ipv4.addresses = [ { address = "192.168.1.23"; prefixLength = 24; } ];
     };
