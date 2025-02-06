32ceed3318e1729062052b467d69c8781291e71f
Joey Hafner <joey@jafner.net>
2025-02-05 23:17:50 -0800

    Fix: Restore sshKeyPaths to use the user's key instead of root's.

--- dotfiles/modules/sops.nix
+++ dotfiles/modules/sops.nix
@@ -1,7 +1,7 @@
 { sys, pkgs, inputs, flake, ... }: {
   imports = [ inputs.sops-nix.nixosModules.sops ]; 
   sops = {
-    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    age.sshKeyPaths = [ "${sys.ssh.path}/${sys.ssh.privateKey}" ];
     #age.keyFile = "/home/${sys.username}/.config/sops/age/keys.txt"; # This file is expected to be provided from outside the nix-store
     age.generateKey = false;
   };
