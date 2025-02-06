c868343c42f6c58c389bdb96146333cc8524c37d
Joey Hafner <joey@jafner.net>
2025-02-06 13:25:24 -0800

    Enhancement: Update hardware and networking config.   - Remove not-detected.nix.   - Set hardware configs explicitly.   - Configure 32GB swapfile.   - Split networking config into networking.nix.   - Disable DHCP, set interface address explicitly.

---
+++ dotfiles/systems/fighter/networking.nix
@@ -0,0 +1,12 @@
+{ sys, ... }: {
+  networking = {
+    hostName = "fighter";
+    defaultGateway = { address = "192.168.1.1"; interface = "enp3s0"; };
+    interfaces."enp3s0" = {
+      useDHCP = false;
+      macAddress = "00:02:c9:56:bf:9a";
+      ipv4.addresses = [ { address = "192.168.1.23"; prefixLength = 24; } ];
+    };
+    nameservers = [ "10.0.0.1" ];
+  };
+}
