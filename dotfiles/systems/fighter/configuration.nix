366039d2739fb31ca33c7ab1330b21e0ee0cdc8d
Joey Hafner <joey@jafner.net>
2025-02-07 13:02:27 -0800

    Enhancement: Add localization, basic nixpkgs config.

--- dotfiles/systems/fighter/configuration.nix
+++ dotfiles/systems/fighter/configuration.nix
@@ -11,6 +11,24 @@
     ../../modules/sops.nix
   ];
 
+  nixpkgs.config.allowUnfree = true;
+  programs.nix-ld.enable = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  time.timeZone = "America/Los_Angeles";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
   systemd.enableEmergencyMode = false;
   # DO NOT CHANGE
   system.stateVersion = "24.11";
