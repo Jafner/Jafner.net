7191a5a3bf3c05f90965d8e8462e81ea764f6836
Joey Hafner <joey@jafner.net>
2025-02-27 17:17:46 -0800

    Fix: Change default log driver from nixos' default, "journald" to docker's default. May mitigate systemd becoming unresponsive after some time.

--- dotfiles/modules/docker.nix
+++ dotfiles/modules/docker.nix
@@ -2,6 +2,7 @@
   virtualisation.docker = {
     enable = true;
     daemon.settings.data-root = "/docker";
+    logDriver = "local";
     rootless.enable = false; 
     rootless.setSocketVariable = true;
   };
