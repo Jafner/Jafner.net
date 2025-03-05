9170227e1e55b97cdf9769bc749c034259277c53
Joey Hafner <joey@jafner.net>
2025-03-05 13:02:11 -0800

    Fix: Disable all stacks on artificer except gitea-runner.

--- dotfiles/systems/artificer/stacks.nix
+++ dotfiles/systems/artificer/stacks.nix
@@ -1,10 +1,5 @@
 { ... }: {
   imports = let stacksPath = ../../../homelab/stacks; in [
-    "${stacksPath}/gitea/stack.nix"
     "${stacksPath}/gitea-runner/stack.nix"
-    "${stacksPath}/keycloak/stack.nix"
-    "${stacksPath}/monitoring/stack.nix"
-    "${stacksPath}/traefik/stack.nix"
-    "${stacksPath}/wireguard/stack.nix"
   ];
 }
