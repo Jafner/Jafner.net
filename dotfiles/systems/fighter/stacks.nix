c21c79fb5abff4fc36ddeeb61f3dbd3318de25e6
Joey Hafner <joey@jafner.net>
2025-03-05 13:09:44 -0800

    Feature: Add gitea-runner to fighter.

--- dotfiles/systems/fighter/stacks.nix
+++ dotfiles/systems/fighter/stacks.nix
@@ -2,6 +2,7 @@
   imports = let stacksPath = ../../../homelab/stacks; in [
     "${stacksPath}/ai/stack.nix"
     "${stacksPath}/autopirate/stack.nix"
+    "${stacksPath}/gitea-runner/stack.nix"
     "${stacksPath}/homeassistant/stack.nix"
     "${stacksPath}/keycloak/stack.nix"
     "${stacksPath}/manyfold/stack.nix"
