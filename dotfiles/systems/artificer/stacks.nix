a516b6a8a7f18dd4e66f75928d6fbf458e7e14e1
Joey Hafner <joey@jafner.net>
2025-03-05 18:19:13 -0800

    Fix: Add artificer's pubkey to traefik secrets. Add traefik to artificer's stacks.

--- dotfiles/systems/artificer/stacks.nix
+++ dotfiles/systems/artificer/stacks.nix
@@ -1,5 +1,7 @@
 { ... }: {
   imports = let stacksPath = ../../../homelab/stacks; in [
     "${stacksPath}/gitea-runner/stack.nix"
+    "${stacksPath}/monitoring/stack.nix"
+    "${stacksPath}/traefik/stack.nix"
   ];
 }
