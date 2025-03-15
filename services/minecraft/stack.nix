d2ac99ac23d8f4f917c41dc5fc0a718418187adb
Joey Hafner <joey@jafner.net>
2025-03-15 14:01:59 -0700

    Enhancement: Experimentally initialize systemd service for minecraft stack.
    Former-commit-id: 7a715e9110ff519df83d4e0939392140335551b3

--- services/minecraft/stack.nix
+++ services/minecraft/stack.nix
@@ -14,6 +14,27 @@
       target = "stacks/${stack}/.env";
     };
   };
+  systemd.services."minecraft-server" = {
+    requires = [ "docker.service" ];
+    after = [ "docker.service" ];
+    serviceConfig = {
+      Restart = "no";
+      User = "${sys.username}";
+      Group = "docker";
+      TimeoutStopSec = "120";
+      WorkingDirectory = "/home/${sys.username}/stacks/${stack}";
+      ExecStartPre = "docker compose down";
+      ExecStart = "docker compose up";
+      ExecStop = "docker compose down";
+    };
+  };
+  systemd.timers."minecraft-server-restart" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 06:00:00";
+      Unit = "minecraft-server.service";
+    };
+  };
   environment.systemPackages = [ pkgs.minecraft-server ];
   networking.firewall.allowedTCPPorts = [ 25565 ];
   networking.firewall.allowedUDPPorts = [ 25565 ];
