35e1d5bc8abd0cd268f423633b446b8539252598
Joey Hafner <joey@jafner.net>
2025-03-31 14:50:36 -0700

    Feature: Refactor stacks/n8n.

--- modules/stacks/n8n/stack.nix
+++ modules/stacks/n8n/stack.nix
@@ -1,17 +1,12 @@
-{ pkgs, config, ... }: let stack = "n8n"; in let cfg = config.modules.stacks.${stack}; in {
-  options = with pkgs.lib; {
-    modules.stacks.${stack} = {
+{ pkgs, lib, config, username, ... }: with lib; let stack = "n8n"; in let cfg = config.stacks.${stack}; in {
+  options = {
+    stacks.${stack} = {
       enable = mkEnableOption "${stack}";
       username = mkOption {
         type = types.str;
         default = "admin";
         description = "Username of the default, primary user.";
         example = "john";
-      };
-      secretsFile = mkOption {
-        type = types.pathInStore;
-        default = null;
-        description = "Path to the stack's sops-nix-encrypted secrets file.";
       };
       paths = mkOption {
         type = types.submodule {
@@ -43,51 +38,32 @@
     };
   };
   config =  pkgs.lib.mkIf cfg.enable  {
-    sops.secrets."${stack}" = {
-      sopsFile = cfg.secretsFile;
-      key = "";
-      mode = "0440";
-      owner = cfg.username;
-    };
-    home-manager.users."${cfg.username}".home.file = {
+    home-manager.users."${username}".home.file = {
       "${stack}/docker-compose.yml" = {
         enable = true;
         text = ''
           services:
-            ${stack}:
-              image: image:tag
-              container_name: ${stack}_service
-              restart: "no"
+            n8n:
+              image: docker.n8n.io/n8nio/n8n:latest
+              container_name: n8n_n8n
+              restart: "unless-stopped"
               networks:
                 web:
               volumes:
-                - ${cfg.paths.appdata}:/data
-              env_file:
-                - path: /run/secrets/${stack}
-                  required: true
+                - ${cfg.paths.appdata}:/home/node/.n8n
+              labels:
+                - traefik.http.routers.n8n.rule=Host(`${cfg.domains.${stack}}`)
+                - traefik.http.routers.n8n.tls.certresolver=lets-encrypt
+              environment:
+                - N8N_EDITOR_BASE_URL=https://${cfg.domains.${stack}}/
+                - WEBHOOK_URL=https://${cfg.domains.${stack}}/
+
           networks:
             web:
               external: true
         '';
         target = "stacks/${stack}/docker-compose.yml";
       };
-    };
-  };
-}
-
-
-{ sys, stacks, ... }: let stack = "n8n"; in {
-  home-manager.users."${sys.username}".home.file = {
-    "${stack}" = {
-      enable = true;
-      recursive = true;
-      source = ./.;
-      target = "stacks/${stack}/";
-    };
-    "${stack}/.env" = {
-      enable = true;
-      text = ''APPDATA=${stacks.appdata}/${stack}'';
-      target = "stacks/${stack}/.env";
     };
   };
 }
