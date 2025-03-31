b851e93f65ad00ddab7d1f4765e2c4a6ad6da978
Joey Hafner <joey@jafner.net>
2025-03-31 14:14:01 -0700

    Feature: Refactor stacks/send.

--- modules/stacks/send/stack.nix
+++ modules/stacks/send/stack.nix
@@ -1,17 +1,12 @@
-{ pkgs, config, ... }: let stack = "send"; in let cfg = config.modules.stacks.${stack}; in {
+{ pkgs, lib, config, username, ... }: with lib; let stack = "send"; in let cfg = config.stacks.${stack}; in {
   options = with pkgs.lib; {
-    modules.stacks.${stack} = {
+    stacks.${stack} = {
       enable = mkEnableOption "${stack}";
       username = mkOption {
         type = types.str;
         default = "admin";
         description = "Username of the default, primary user.";
         example = "john";
-      };
-      secretsFile = mkOption {
-        type = types.pathInStore;
-        default = null;
-        description = "Path to the stack's sops-nix-encrypted secrets file.";
       };
       paths = mkOption {
         type = types.submodule {
@@ -43,51 +38,51 @@
     };
   };
   config =  pkgs.lib.mkIf cfg.enable  {
-    sops.secrets."${stack}" = {
-      sopsFile = cfg.secretsFile;
-      key = "";
-      mode = "0440";
-      owner = cfg.username;
-    };
-    home-manager.users."${cfg.username}".home.file = {
+    home-manager.users."${username}".home.file = {
       "${stack}/docker-compose.yml" = {
         enable = true;
         text = ''
           services:
-            ${stack}:
-              image: image:tag
-              container_name: ${stack}_service
+            send:
+              image: registry.gitlab.com/timvisee/send:latest
+              user: 1000:1000
+              container_name: send_send
               restart: "no"
               networks:
-                web:
+                - send
+                - web
+              environment:
+                VIRTUAL_HOST: 0.0.0.0
+                VIRTUAL_PORT: 1234
+                DHPARAM_GENERATION: false
+                NODE_ENV: production
+                BASE_URL: https://${cfg.domains.${stack}}
+                PORT: 1234
+                REDIS_HOST: redis
+                FILE_DIR: /uploads
+                MAX_FILE_SIZE: 42949672960 # 40 GiB
               volumes:
-                - ${cfg.paths.appdata}:/data
-              env_file:
-                - path: /run/secrets/${stack}
-                  required: true
+                - ${cfg.paths.appdata}/uploads:/uploads
+              labels:
+                - traefik.http.routers.send.rule=Host(`${cfg.domains.${stack}}`)
+                - traefik.http.routers.send.tls.certresolver=lets-encrypt-dns01
+                - traefik.http.routers.send.tls.options=tls12@file
+                - traefik.http.services.send.loadbalancer.server.port=1234
+
+            redis:
+              image: 'redis:alpine'
+              container_name: send_redis
+              networks:
+                - send
+              restart: "no"
+
           networks:
             web:
               external: true
+            send:
         '';
         target = "stacks/${stack}/docker-compose.yml";
       };
-    };
-  };
-}
-
-
-{ sys, stacks, ... }: let stack = "send"; in {
-  home-manager.users."${sys.username}".home.file = {
-    "${stack}" = {
-      enable = true;
-      recursive = true;
-      source = ./.;
-      target = "stacks/${stack}/";
-    };
-    "${stack}/.env" = {
-      enable = true;
-      text = ''APPDATA=${stacks.appdata}/${stack}'';
-      target = "stacks/${stack}/.env";
     };
   };
 }
