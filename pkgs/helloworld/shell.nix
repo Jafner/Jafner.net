d13838cf7c5ce41b33b16ceea3ba41a9cd8e6399
Joey Hafner <joey@jafner.net>
2025-04-09 20:34:43 -0700

    WIP: helloworld shell, gitignore

---
+++ pkgs/helloworld/shell.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> {} }: pkgs.mkShell {
+  name = "helloworld";
+  src = pkgs.callPackage ./pkg.nix {  };
+  nativeBuildInputs = with pkgs; [
+    rustc
+    cargo
+  ];
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
