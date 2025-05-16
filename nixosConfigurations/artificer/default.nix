8c6b211d5681ae8f2ce5dfb259449ca3a2265d00
Joey Hafner <joey@jafner.net>
2025-05-16 13:48:07 -0700

    Add tailscale

--- nixosConfigurations/artificer/default.nix
+++ nixosConfigurations/artificer/default.nix
@@ -10,6 +10,8 @@
     ./vaultwarden.nix
   ];
 
+  services.tailscale.enable = true;
+
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/f222513b-ded1-49fa-b591-20ce86a2fe7f";
     fsType = "ext4";
