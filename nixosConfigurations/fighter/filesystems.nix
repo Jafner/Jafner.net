7d161576158d162381c1181a9fb2a7480bb44c89
Joey Hafner <joey@jafner.net>
2025-09-18 08:30:27 -0700

    Re-implement SMB shares
    Starting with AV

--- nixosConfigurations/fighter/filesystems.nix
+++ nixosConfigurations/fighter/filesystems.nix
@@ -1,4 +1,9 @@
-{...}: {
+{username, ...}: {
+  sops.secrets."paladin/smb" = {
+    sopsFile = ../../secrets/truenas.secrets.yml;
+    mode = "0440";
+    owner = username;
+  };
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/88a3f223-ed42-4be1-a748-bb9e0f9007dc";
     fsType = "ext4";
@@ -17,4 +22,17 @@
       size = 32 * 1024;
     }
   ];
+
+  fileSystems."/mnt/av" = {
+    enable = true;
+    mountPoint = "/mnt/av";
+    device = "//192.168.1.12/AV";
+    fsType = "cifs";
+    options = [
+      "vers=3"
+      "credentials=/run/secrets/paladin/smb"
+      "uid=1000,forceuid"
+      "gid=1000,forcegid"
+    ];
+  };
 }
