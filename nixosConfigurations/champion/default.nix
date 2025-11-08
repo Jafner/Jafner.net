3384fdf440ad8a36293f8dd1340f0b62b1600a57
Jafner <40403594+Jafner@users.noreply.github.com>
2025-11-07 16:07:57 -0800

    Add kernel modules for WireGuard and nft_masq

--- nixosConfigurations/champion/default.nix
+++ nixosConfigurations/champion/default.nix
@@ -26,7 +26,10 @@
   # Other options:
   # - https://mynixos.com/nixpkgs/packages/linuxKernel.packages
   # - https://mynixos.com/nixpkgs/packages/linuxPackages
-
+  boot.kernelModules = [
+    wireguard
+    nft_masq
+  ];
   environment.etc."current-nixos".source = ../../.;
   environment.systemPackages = with pkgs; [
     coreutils
